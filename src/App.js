87a0d4bef10b6e5fb7d734ef0d1b94608bd5916e
abusiddik95 <siddikabu34@gmail.com>
2020-04-21 21:34:53 -0400

    React chart using rechart

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SimpleLineChart from './Components/SimpleLineChart/SimpleLineChart';
+import TwoLevelPieChart from './Components/TwoLevelPieChart/TwoLevelPieChart';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>React Chart</h2>
+      <SimpleLineChart></SimpleLineChart>
+      <TwoLevelPieChart></TwoLevelPieChart>
     </div>
   );
 }
